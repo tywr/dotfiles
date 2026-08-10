cb3a52c2e0fad5dc930fd5fc9a84c4731cf7e619
tywr <tanguy.viv@gmail.com>
2026-08-10 16:46:05 +0200

    fix: FZF style

--- .config/zsh/env.zsh
+++ .config/zsh/env.zsh
@@ -6,7 +6,7 @@ export XDG_CONFIG_HOME="$HOME/.config"
 export EDITOR=nvim
 export VISUAL="$EDITOR"
 export FZF_DEFAULT_OPTS="-e \
-    --color fg:248,bg:-1,hl:6 \
+    --color fg:250,bg:-1,hl:2 \
     --color fg+:7:regular,hl+:2:bold,bg+:0 \
     --color prompt:15,pointer:6,marker:6,spinner:5,info:2,header:2,border:8 \
     --bind 'tab:toggle-down,btab:toggle-up' \
